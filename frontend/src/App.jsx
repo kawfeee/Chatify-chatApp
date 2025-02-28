0edb46512730d070fa480234e9199e56e3bba9dd
kawfeee <kaifalikhan347@gmail.com>
2025-02-28 19:36:41 +0530

    themes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,10 +12,12 @@ import { useEffect } from 'react';
 
 import {Loader} from "lucide-react"
 import { Toaster } from 'react-hot-toast';
+import { useThemeStore } from './store/useThemeStore';
 
 
 const App = () => {
   const {authUser, checkAuth, isCheckingAuth} = useAuthStore()
+  const {theme} = useThemeStore()
 
   useEffect(() => {
     checkAuth()
@@ -31,7 +33,7 @@ const App = () => {
   
 
   return (
-    <div>
+    <div data-theme={theme}>
       <Navbar />
 
       <Routes>
